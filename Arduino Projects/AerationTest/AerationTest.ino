4c0ad695ada19be44c8d9dcec091aefcb1229777
Michael Osthege <zufallsprinzip@hotmail.de>
2015-09-08 23:17:24 +0200

    aeration test going at 7200000 sph - overflow check not implemented yet

--- Arduino Projects/AerationTest/AerationTest.ino
+++ Arduino Projects/AerationTest/AerationTest.ino
@@ -1,8 +1,20 @@
-#define Step 13
+﻿#define Step 13
 #define Dir 12
-long now = 0;
+unsigned long now = 0;
 long last = 0;
 long period_trigger   = 1000;
+
+
+// setpoints
+float sph = 7200000;
+// reference values that have to be reset
+long t_R = 0;
+long delta_t = 0;
+long s_done = 0;
+// values that are calculated in every loop:
+unsigned long s_sofar = 0;
+int s_diff = 0;
+//
 
 void setup() {
 	// put your setup code here, to run once:
@@ -14,8 +26,9 @@ void setup() {
 
 void loop()
 {
-	now = micros(); // this is the reference time when the iteration began
-	MakeSteps();
+	now = millis(); // this is the reference time when the iteration began
+	MakeComplexSteps();
+	delay(0);
 }
 void MakeSteps()
 {
@@ -31,6 +44,43 @@ void MakeSteps()
 		last = now; // save when the last signal was given
 	}
 }
+
+
+void MakeComplexSteps()
+{
+	delta_t = calcElapsedTime(t_R, now); // time since the interval began
+	s_sofar = sph * delta_t / 3600000 - 1; // steps that should have been completed until now
+	s_diff = s_sofar - s_done; // steps we are lagging behind
+
+	Serial.print("delta_t=");
+	Serial.print(delta_t);
+
+	Serial.print(" ... s_done=");
+	Serial.print(s_done);
+
+	Serial.print(" ... s_sofar=");
+	Serial.print(s_sofar);
+
+	Serial.print(" ... s_diff=");
+	Serial.print(s_diff);
+
+	Serial.println();
+	//TODO: what happens when delta_t overflows?
+	//      find the correct time when to reset t_R, s_done
+
+	for (int i = 0; i < s_diff; i++)
+	{
+		digitalWrite(Step, HIGH);
+		delayMicroseconds(10);
+		digitalWrite(Step, LOW);
+		delayMicroseconds(10);
+		s_done++; // count all steps that were executed
+	}
+}
+
+
+
+
 long calcElapsedTime(long before, long after)
 {
 	if (after >= before)
