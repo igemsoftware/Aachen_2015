ace63e3b474b1377d837d76342030e0d8ec4b45a
Michael Osthege <zufallsprinzip@hotmail.de>
2015-09-13 18:40:14 +0200

    protocol puppet

---
+++ ProtocolPuppet/Converters.cs
@@ -0,0 +1,52 @@
+﻿using MCP.Protocol;
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Windows;
+using System.Windows.Data;
+
+namespace ProtocolPuppet
+{
+    public class UIConverter : IValueConverter
+    {
+        private static char[] InvalidFileNameCharacters = System.IO.Path.GetInvalidFileNameChars();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            switch (parameter as string)
+            {
+                case "BaudRate":
+                    if (value is int)
+                        return (BaudRate)value;
+                    else
+                        return (int)value;
+                case "IsLegalFilenameString":
+                    if (string.IsNullOrWhiteSpace(value as string))
+                        return false;
+                    foreach (char c in value as string)
+                        if (InvalidFileNameCharacters.Contains(c))
+                            return false;
+                    return true;
+                case "BoolToVisibility": return ((bool)value == true) ? Visibility.Visible : Visibility.Collapsed;
+                case "IsNotNull": return (value != null);
+                case "IsNull": return (value == null);
+                default:
+                    return null;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            switch (parameter as string)
+            {
+                case "BaudRate":
+                    if (value is int)
+                        return (BaudRate)value;
+                    else
+                        return (int)value;
+                default:
+                    return null;
+            }
+        }
+    }
+}
---
+++ ProtocolPuppet/ViewModel.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TCD.Controls;
+using MCP.Protocol;
+using TCD;
+
+namespace ProtocolPuppet
+{
+    public class ViewModel : PropertyChangedBase
+    {
+        private SerialIO _PrimarySerial = new SerialIO();
+        public SerialIO PrimarySerial { get { return _PrimarySerial; } set { _PrimarySerial = value; OnPropertyChanged(); } }
+        
+        private RelayCommand _SendMessageCommand;
+        public RelayCommand SendMessageCommand { get { return _SendMessageCommand; } set { _SendMessageCommand = value; OnPropertyChanged(); } }
+
+        private ParticipantID _From;
+        public ParticipantID From { get { return _From; } set { _From = value; OnPropertyChanged(); } }
+
+        private ParticipantID _To;
+        public ParticipantID To { get { return _To; } set { _To = value; OnPropertyChanged(); } }
+
+        private MessageType _Type;
+        public MessageType Type { get { return _Type; } set { _Type = value; OnPropertyChanged(); } }
+        
+			
+
+        private string _Contents;
+        public string Contents { get { return _Contents; } set { _Contents = value; OnPropertyChanged(); } }
+
+        private string _MessageLog;
+        public string MessageLog { get { return _MessageLog; } set { _MessageLog = value; OnPropertyChanged(); } }
+        
+			
+
+        public ViewModel()
+        {
+            PrimarySerial.NewMessageReceived += PrimarySerial_NewMessageReceived;
+            SendMessageCommand = new RelayCommand(delegate
+            {
+                SendMessage();
+            });
+        }
+        private void PrimarySerial_NewMessageReceived(object sender, Message message)
+        {
+            try
+            {
+                MessageLog += message.ToString() + "\r\n";
+            }
+            catch
+            {
+
+            }
+
+        }
+
+        private void SendMessage()
+        {
+            try
+            {
+                Message msg = new Message(From, To, Type, Contents.Split(new string[] { "\\t" }, StringSplitOptions.None));
+                PrimarySerial.SendMessage(msg);
+                MessageLog += msg.ToString() + "\r\n";
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
